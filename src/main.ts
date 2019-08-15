a38360849814396d10e0ffa7cabd818709d2ac9a
839 <8398a7@gmail.com>
2019-08-16 00:49:47 +0900

    send a message

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import * as core from '@actions/core';
+import { Send } from './slack';
 
 async function run() {
   try {
     const text = core.getInput('text');
     const iconEmoji = core.getInput('iconEmoji');
+    await Send({
+      text: text,
+      icon_emoji: iconEmoji,
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
