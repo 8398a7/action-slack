6e358dc837245080af75d7748c53281022c3453d
839 <8398a7@gmail.com>
2019-12-31 15:47:29 +0900

    [#34] [command] npm run release

--- dist/index.js
+++ dist/index.js
@@ -10376,16 +10376,17 @@ class Client {
     payloadTemplate() {
         return __awaiter(this, void 0, void 0, function* () {
             const text = this.mentionText(this.with.mention);
+            const { username, icon_emoji, icon_url, channel } = this.with;
             return {
                 text,
+                username,
+                icon_emoji,
+                icon_url,
+                channel,
                 attachments: [
                     {
                         color: '',
                         author_name: this.with.author_name,
-                        username: this.with.username,
-                        icon_emoji: this.with.icon_emoji,
-                        icon_url: this.with.icon_url,
-                        channel: this.with.channel,
                         fields: yield this.fields(),
                     },
                 ],
