f2e70decd3c77f185029fbc2da65d4d4f9aafbe0
8398a7 <8398a7@gmail.com>
2020-09-23 21:11:31 +0900

    [#108] Add test for renaming job

---
+++ __tests__/customJobName.test.ts
@@ -0,0 +1,67 @@
+import nock from 'nock';
+
+process.env.GITHUB_RUN_ID = '2';
+process.env.MATRIX_CONTEXT = '{"os": "ubuntu-18.04"}';
+
+import { setupNockCommit, setupNockJobs, successMsg } from './helper';
+import { Client, With, Success } from '../src/client';
+
+beforeAll(() => {
+  nock.disableNetConnect();
+  setupNockCommit(process.env.GITHUB_SHA as string);
+  setupNockJobs(
+    process.env.GITHUB_RUN_ID as string,
+    'actions.custom-job-name.jobs',
+  );
+});
+afterAll(() => {
+  nock.cleanAll();
+  nock.enableNetConnect();
+});
+
+describe('job_name', () => {
+  beforeEach(() => {
+    process.env.GITHUB_REPOSITORY = '8398a7/action-slack';
+    process.env.GITHUB_EVENT_NAME = 'push';
+    const github = require('@actions/github');
+    github.context.payload = {};
+  });
+
+  it('works even if you rename the job', async () => {
+    const withParams: With = {
+      status: Success,
+      mention: '',
+      author_name: '',
+      if_mention: '',
+      username: '',
+      icon_emoji: '',
+      icon_url: '',
+      channel: '',
+      fields: 'job,took',
+      job_name: 'Custom Job',
+    };
+    const client = new Client(withParams, process.env.GITHUB_TOKEN, '');
+    expect(await client.prepare('')).toStrictEqual({
+      text: successMsg,
+      attachments: [
+        {
+          author_name: '',
+          color: 'good',
+          fields: [
+            {
+              short: true,
+              title: 'job',
+              value:
+                '<https://github.com/8398a7/action-slack/runs/762195612|Custom Job (ubuntu-18.04)>',
+            },
+            { short: true, title: 'took', value: '1 hour 1 min 1 sec' },
+          ],
+        },
+      ],
+      username: '',
+      icon_emoji: '',
+      icon_url: '',
+      channel: '',
+    });
+  });
+});
